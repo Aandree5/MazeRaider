210de17789c3ee7f6152992d16320d90cbe3fc00
Jasper Dahil (dahilj) <dahilj@coventry.ac.uk>
2018-01-30 19:01:09 +0000

    Added surrounding detection and moving the player (not run) - Build problem
    made an array that Andre will use to determine the options of where the player will go
    I cannot run the code ... I dont know why

--- src/Player.cpp
+++ src/Player.cpp
@@ -1,12 +1,6 @@
 #include "Player.h"
 #include <iostream>
 
-/*
-
-
-
-*/
-
 // Sorry forgot about it, this one needs to me here, it is the main function of your class, the one that is called
 // when we instantiate an object of the class in main.cpp
 // usually where you would define the default values of the variables
@@ -14,22 +8,91 @@ Player::Player()
 {
 }
 
-void Player::move(char direction)
+void Player::checkSurrounding(int x, int y)
 {
+    xPos = x;
+    yPos = y;
     //check surroundings
-    /*if(get<0>(maze->getDataMWH())[xPos - 1][yPos]=1)
-        leftM = false;
+    if(get<0>(maze->getDataMWH())[xPos - 1][yPos]=1)
+    {
+        pathFinder[2] = 1;
+    }else{
+        pathFinder[2] = 0;
+    }
+
     if(get<0>(maze->getDataMWH())[xPos + 1][yPos]=1)
-        rightM = false;
-        */
-    //move the player
+    {
+        pathFinder[3] = 1;
+    }else{
+        pathFinder[3] = 0;
+    }
 
-    std::cout << direction << std::endl;
-
-    //give options
+    if(get<0>(maze->getDataMWH())[xPos][yPos - 1]=1)
+    {
+        pathFinder[0] = 1;
+    }else{
+        pathFinder[0] = 0;
+    }
+    if(get<0>(maze->getDataMWH())[xPos][yPos + 1]=1)
+    {
+        pathFinder[1] = 1;
+    }else{
+        pathFinder[1] = 0;
+    }
 }
 
-
+void Player::movePlayer(int direction)
+{
+    int steps = 0;
+    if(direction==2)
+    {
+        while(get<0>(maze->getDataMWH())[xPos][yPos+1]!=1) //not yet reached a wall
+        {
+            if(get<0>(maze->getDataMWH())[xPos-1][yPos]=1 || get<0>(maze->getDataMWH())[xPos+1][yPos]=1) //there's a path from either side
+            {
+                checkSurrounding(xPos, yPos+steps);
+                break;
+            }
+            steps+=1;
+        }
+    }else if(direction==4)
+    {
+        while(get<0>(maze->getDataMWH())[xPos-1][yPos]!=1) //not yet reached a wall
+        {
+            if(get<0>(maze->getDataMWH())[xPos][yPos+1]=1 || get<0>(maze->getDataMWH())[xPos][yPos-1]=1) //there's a path from either side
+            {
+                checkSurrounding(xPos+steps, yPos);
+                steps = 0;
+                break;
+            }
+            steps-=1;
+        }
+    }else if(direction==6)
+    {
+        while(get<0>(maze->getDataMWH())[xPos+1][yPos]!=1) //not yet reached a wall
+        {
+            if(get<0>(maze->getDataMWH())[xPos][yPos+1]=1 || get<0>(maze->getDataMWH())[xPos][yPos-1]=1) //there's a path from either side
+            {
+                checkSurrounding(xPos+steps, yPos);
+                steps = 0;
+                break;
+            }
+            steps+=1;
+        }
+    }else if(direction==8)
+    {
+        while(get<0>(maze->getDataMWH())[xPos][yPos-1]!=1) //not yet reached a wall
+        {
+            if(get<0>(maze->getDataMWH())[xPos-1][yPos]=1 || get<0>(maze->getDataMWH())[xPos+1][yPos]=1) //there's a path from either side
+            {
+                checkSurrounding(xPos, yPos+steps);
+                steps = 0;
+                break;
+            }
+            steps-=1;
+        }
+    }
+}
 
 
 void Player::spawnPlayer(int x, int y)
