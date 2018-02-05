524098fe2f04d5ca6871ea4b09ec6ffca89f78ea
Delia-Florenta Manea (manead) <manead@coventry.ac.uk>
2018-02-05 18:03:51 +0000

    Added LevelManager File (Not in project yet)

---
+++ src/LevelManager.cpp
@@ -0,0 +1,14 @@
+#include "LevelManager.h"
+
+LevelManager::LevelManager()
+{
+    ui = new UI(this);
+    maze = new Maze(31, 31);
+    player = new Player();
+
+    player->maze = maze;
+    player->xPos = maze->getMazeStart().first;
+    player->yPos = maze->getMazeStart().second;
+
+    ui->ShowUI(player);
+}
