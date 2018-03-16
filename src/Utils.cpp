eaabfec94ea9139220341974bdd6d16f7eede9ac
Jesse Prescott <bleedobsidian@gmail.com>
2018-03-16 13:00:06 +0000

    Add more comments.

--- src/Utils.cpp
+++ src/Utils.cpp
@@ -1,5 +1,14 @@
 #include "Utils.h"
 
+/**
+ * A class of static utility functions.
+ *
+ * @author Jesse Prescott.
+ */
+
+ /**
+  * Creates a 2D integer array of given width, height and initial values.
+  */
 int** Utils::create2DIntArray(unsigned width, unsigned height, int value) {
 
     // Initialise 2D array.
@@ -21,12 +30,21 @@ int** Utils::create2DIntArray(unsigned width, unsigned height, int value) {
     return array2D;
 }
 
+/**
+ * Deletes a given 2D integer array to clear memory.
+ */
 void Utils::delete2DIntArray(unsigned width, int** &array2D) {
 
+    // For every inner array.
     for(int w = 0; w < width; w++) {
+
+        // Delete it.
         delete [] array2D[w];
     }
 
+    // Delete parent array.
     delete [] array2D;
+
+    // Set to 0.
     array2D = 0;
 }
